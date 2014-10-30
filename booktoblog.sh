32c1330810ed98292b104a9db19070eb9e55b6a6
Ørjan Vøllestad <orjanv@gmail.com>
2014-10-30 22:25:14 +0100

    updated and removed a file

--- booktoblog.sh
+++ booktoblog.sh
@@ -2,7 +2,8 @@
 #
 # booktoblog.sh is a bash utility to download the ascii version of a book
 # and find haikus in it using the findhaikus program.
-# The script takes a file as an argument, with book ids in it on each line
+# The script takes a file as an argument, with book ids in it on each line.
+# Then by using googlecl, blog the result to a Blogger blog. 
 #
 # For example:
 # $ ./booktoblog.sh filename.txt
@@ -15,49 +16,56 @@ _file="$1"
 
 if [ -s "$_file" ]
 then
-    # Get the top book id
-    _id=$(head -1 $_file)
+	# Get the top book id
+	_id=$(head -1 $_file)
 
-    # download the book based on the book ID number given as args, for ex: 2591
-    wget -O book http://www.gutenberg.org/files/$_id/$_id.txt
+	# download the book based on the book ID number given as args, for ex: 2591
+	wget -O book http://www.gutenberg.org/files/$_id/$_id.txt
 
-    # setting some variables
-    AUTHOR=$(grep "Author:" book | sed 's/Author: //g' | strings)
-    TITLE=$(grep "Title:" book | sed 's/Title: //g' | strings)
-    echo $TITLE
-    FOLDER=$(echo $TITLE-by-$AUTHOR | sed 's/ /_/g')
+	# setting some variables
+	AUTHOR=$(grep "Author:" book | sed 's/Author: //g' | strings)
+	TITLE=$(grep "Title:" book | sed 's/Title: //g' | strings)
+	echo $TITLE
+	FOLDER=$(echo $TITLE-by-$AUTHOR | sed 's/ /_/g')
 
-    # find cover from goodreads, using a python script
-    IMAGE_URL=$(python goodreads-lookup.py "$TITLE by $AUTHOR" > image_url)
-	echo "cover downloaded based on title only"
+	# find cover from google books, using a python script
+	python openlibrary-cover.py $TITLE $AUTHOR > thumb_desc
+	head -1 thumb_desc > image_url
+	sed -n '2,$p' <thumb_desc > description
 
-    # run it through findhaikus program
-    touch haikus
-    findhaikus book > haikus
+	# run it through findhaikus program
+	/usr/local/bin/findhaikus book > haikus
+	sleep 15 && killall findhaikus > /dev/null 2>&1 & 
 
 	# if any haikus found, blog it. If not, do nothing.
-    if [ -s haikus ]
-    then    
+	if [ -s haikus ]
+	then    
 		# replace () in haikus file with empty lines
-		sed -i 's/()//g' haikus
+ 		sed -i 's/()//g' haikus
 
 		# insert image into blogentry
-		echo "<img src='$(cat image_url)' align='right'>" | cat - haikus > temp && mv temp haikus
+		echo "<img src='$(cat image_url)' align='right'><p><b>About the book:</b></p><p>$(cat description)</p>" | cat - haikus > temp && mv temp haikus
 
 		# blog the haikus to blogger
 		google blogger post --tags "haiku" --title "Haikus from $TITLE by $AUTHOR" --src haikus
-
+		echo "posted on blogger"
+	else
+		echo "no haikus found"
 		# remove book id from file, create folder for book and move away all files
 		mkdir $FOLDER
 		mv -t $FOLDER book haikus image_url
 		sed -i '1d' $_file
-	else
-		echo "no haikus found"
+		# run the script again if no haikus found
+		booktoblog.sh $_file 
 		exit 1
 	fi
-
+	# remove book id from file, create folder for book and move away all files
+	mkdir $FOLDER
+	mv -t $FOLDER book haikus image_url
+	sed -i '1d' $_file
+	exit 1
 else
-    echo "$_file is empty."
-    exit 1
+	echo "$_file is empty."
+	exit 1
 fi
 
